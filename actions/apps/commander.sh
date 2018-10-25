8b317424e08175378d587f583abc927de426ee25
Georgi Stoyanov <33185290+geops22@users.noreply.github.com>
2018-10-25 13:59:43 -0500

    Text Change ARK -> SBX

--- actions/apps/commander.sh
+++ actions/apps/commander.sh
@@ -14,7 +14,7 @@ commander_configure_repo ()
     __commander_configure "$choice" "$CORE_DIR" "$CORE_DATA" "$CORE_CONFIG" "$CORE_TOKEN" "$CORE_NETWORK" "$EXPLORER_REPO" "$EXPLORER_DIR"
 
     if [[ -d "$CORE_DIR" ]]; then
-        warning "ARK Core will be pointed to ${CORE_REPO}. This will restart your node."
+        warning "SBX Core will be pointed to ${CORE_REPO}. This will restart your node."
 
         press_to_continue
 
@@ -45,7 +45,7 @@ commander_configure_core_directory ()
     __commander_configure "$CORE_REPO" "$choice" "$CORE_DATA" "$CORE_CONFIG" "$CORE_TOKEN" "$CORE_NETWORK" "$EXPLORER_REPO" "$EXPLORER_DIR"
 
     if [[ -d "$CORE_DIR" ]]; then
-        warning "ARK Core will be stopped and moved to ${CORE_DIR}. This will restart your node."
+        warning "SBX Core will be stopped and moved to ${CORE_DIR}. This will restart your node."
 
         press_to_continue
 
@@ -128,7 +128,7 @@ commander_configure_explorer_repo ()
     __commander_configure "$CORE_REPO" "$CORE_DIR" "$CORE_DATA" "$CORE_CONFIG" "$CORE_TOKEN" "$CORE_NETWORK" "$choice" "$EXPLORER_DIR"
 
     if [[ -d "$EXPLORER_DIR" ]]; then
-        warning "ARK Explorer will be pointed to ${EXPLORER_REPO}. This will restart your explorer."
+        warning "SBX Explorer will be pointed to ${EXPLORER_REPO}. This will restart your explorer."
 
         press_to_continue
 
@@ -159,7 +159,7 @@ commander_configure_explorer_directory ()
     __commander_configure "$CORE_REPO" "$CORE_DIR" "$CORE_DATA" "$CORE_CONFIG" "$CORE_TOKEN" "$CORE_NETWORK" "$EXPLORER_REPO" "$choice"
 
     if [[ -d "$EXPLORER_DIR" ]]; then
-        warning "ARK Explorer will be stopped and moved to ${EXPLORER_DIR}. This will restart your explorer."
+        warning "SBX Explorer will be stopped and moved to ${EXPLORER_DIR}. This will restart your explorer."
 
         press_to_continue
 
